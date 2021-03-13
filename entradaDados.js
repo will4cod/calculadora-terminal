f97799cbe29a5ad540e39bbd4cec2da1d0f1ddc4
will4cod <william4fernandes@gmail.com>
2021-03-13 11:18:56 -0300

    Alterada a entrada de valores do usuario

--- entradaDados.js
+++ entradaDados.js
@@ -1,13 +1,12 @@
 const prompt = require('prompt-sync')();
 
 function entradaUsuario(opcao){
-    let num1 = 0;
-    let num2 = 0;
+    let numeros = [];
     let array = [];
     if(opcao<5){
-        num1 = Number(prompt('Digite o primeiro número: '));
-        num2 = Number(prompt('Digite o segundo número: '));
-        return num1;
+        numeros.push(Number(prompt('Digite o primeiro número: ')));
+        numeros.push(Number(prompt('Digite o segundo número: ')));
+        return numeros;
     }else{
         console.log("OBS: Digite os valores separando-os por traço exemplo: 10-10-10-10-10 ");
         array = prompt('Digite os valores: ');
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let calculadora = require('./calculos');
 console.log('Bem vindo ao sistema\n');
 
 let opcaoMenu = menuEscolha();
+let entrada = [];
 
 while(opcaoMenu > 0){
     if(opcaoMenu > 5){
@@ -14,24 +15,20 @@ while(opcaoMenu > 0){
     }
     
     if(opcaoMenu == 1){
-        let num1 = Number(prompt('Digite o primeiro número: '));
-        let num2 = Number(prompt('Digite o segundo número: '));
-        console.log('Resultado da soma: ' + calculadora.soma(num1,num2));
+        entrada = entradaUsuario(opcaoMenu);
+        console.log('Resultado da soma: ' + calculadora.soma(entrada[0],entrada[1]));
         break;
     }else if(opcaoMenu == 2){
-        let num1 = Number(prompt('Digite o primeiro número: '));
-        let num2 = Number(prompt('Digite o segundo número: '));
-        console.log('Resultado da subtracao: ' + calculadora.subtracao(num1,num2));
+        entrada = entradaUsuario(opcaoMenu);
+        console.log('Resultado da subtracao: ' + calculadora.subtracao(entrada[0],entrada[1]));
         break;
     }else if(opcaoMenu == 3){
-        let num1 = Number(prompt('Digite o primeiro número: '));
-        let num2 = Number(prompt('Digite o segundo número: '));
-        console.log('Resultado da divisao: ' + calculadora.divisao(num1,num2));
+        entrada = entradaUsuario(opcaoMenu);
+        console.log('Resultado da divisao: ' + calculadora.divisao(entrada[0],entrada[1]));
         break;
     }else if(opcaoMenu == 4){
-        let num1 = Number(prompt('Digite o primeiro número: '));
-        let num2 = Number(prompt('Digite o segundo número: '));
-        console.log('Resultado da multiplicacao: ' + calculadora.multiplicacao(num1,num2));
+        entrada = entradaUsuario(opcaoMenu);
+        console.log('Resultado da multiplicacao: ' + calculadora.multiplicacao(entrada[0],entrada[1]));
         break;
     }else if(opcaoMenu == 5){
         console.log('Resultado da média: ' + calculadora.media(entradaUsuario(opcaoMenu)));
